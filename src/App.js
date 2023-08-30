56acb703d227c7a72f93467acc6c57a0d0db0333
malikwaqas150 <malikv137@gmail.com>
2023-08-30 15:51:38 +0500

    first change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js File
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Container from "react-bootstrap/Container";
@@ -13,36 +12,31 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-		// Setting up state
 		this.state = {
 			userInput: "",
 			list: [],
 		};
 	}
 
-	// Set a user input value
 	updateInput(value) {
 		this.setState({
 			userInput: value,
 		});
 	}
 
-	// Add item if user input in not empty
 	addItem() {
 		if (this.state.userInput !== "") {
 			const userInput = {
-				// Add a random id which is used to delete
 				id: Math.random(),
 
-				// Add a user value to list
+				
 				value: this.state.userInput,
 			};
 
-			// Update list
 			const list = [...this.state.list];
 			list.push(userInput);
 
-			// reset state
+		
 			this.setState({
 				list,
 				userInput: "",
@@ -50,14 +44,13 @@ class App extends Component {
 		}
 	}
 
-	// Function to delete item from list use id to delete
+	
 	deleteItem(key) {
 		const list = [...this.state.list];
 
-		// Filter values and leave value which we need to delete
 		const updateList = list.filter((item) => item.id !== key);
 
-		// Update list in state
+		
 		this.setState({
 			list: updateList,
 		});
